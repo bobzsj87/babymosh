75460bdc29ea14c6802a77c99eda2921a94e14b8
Bob Zhou <bobzhou@Bobs-MacBook-Pro.local>
2016-03-15 14:51:20 +0100

    add css

--- generate.js
+++ generate.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
 var ejs = require("ejs");
+var exec = require('child_process').exec;
+var cmd = 'lessc css/less/creative.less css/creative.css';
+exec(cmd, function(error) {
+	if (error) console.error(error);
+	else console.log("finished creative.css")
+});
 
 var template = ejs.compile(fs.readFileSync("./template/_template.html", 'utf8'));
 var files = fs.readdirSync("./template");
@@ -8,5 +14,6 @@ for (var i in files){
 	if (exclude.indexOf(files[i]) == -1) {
 		var filename = files[i];
 		fs.writeFileSync(filename, template({body: fs.readFileSync("./template/"+filename, 'utf8')}));
+		console.log(filename);
 	}
 }
