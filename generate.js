dd0d6c9f472bd5edf937761860f9c72cfafac2cd
Bob Zhou <bobzsj87@gmail.com>
2016-03-23 10:26:02 +0100

    add qiniu cdn

--- generate.js
+++ generate.js
@@ -7,13 +7,16 @@ exec(cmd, function(error) {
 	else console.log("finished creative.css")
 });
 
+var qiniu = "//o42spgxwl.qnssl.com/img/";
+
 var template = ejs.compile(fs.readFileSync("./template/_template.html", 'utf8'));
 var files = fs.readdirSync("./template");
 var exclude = [".DS_Store", "_template.html"];
 for (var i in files){
 	if (exclude.indexOf(files[i]) == -1) {
 		var filename = files[i];
-		fs.writeFileSync(filename, template({body: fs.readFileSync("./template/"+filename, 'utf8')}));
+		var body = fs.readFileSync("./template/"+filename, 'utf8');
+		fs.writeFileSync(filename, template({body: body.replace(/\/img\//g, qiniu)}));
 		console.log(filename);
 	}
 }
